182879e0c69f359b2ce7cd284d63cbb8774c635f
aiti60751-lang <aiti60751@gmail.com>
2026-05-17 22:14:18 +0100

    Fix config.py for Streamlit Cloud

---
+++ utils/config.py
@@ -0,0 +1,11 @@
+﻿import os
+import streamlit as st
+
+def get_groq_key() -> str:
+    key = os.environ.get('GROQ_API_KEY', '')
+    if key:
+        return key
+    try:
+        return st.secrets.get('GROQ_API_KEY', '')
+    except Exception:
+        return ''
